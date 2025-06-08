1ea6937bafa99886cbad0073f2d864439ad12c47
Zonorg <nviscio@gmail.com>
2025-06-08 13:33:39 -0300

    about page children fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { ReactNode } from "react";
-export default function About({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+export default function AboutPage() {
+  return <></>;
 }
